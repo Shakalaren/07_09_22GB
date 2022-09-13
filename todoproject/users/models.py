5006aef645e0344556024e6e672a8553b6a1e217
Shak <Kostuchkovlaf@yandex.ru>
2022-09-13 15:15:42 +0300

    Lesson_1

---
+++ todoproject/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    firstname = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64),
+    birthday_year = models.PositiveIntegerField()
+    email = models.EmailField(unique=True, max_length=254)
